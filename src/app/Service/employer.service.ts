e987d3226eb8afc0930f9f63e8dea0e24c029e56
Jahnavi454 <166401931+Jahnavi454@users.noreply.github.com>
2024-06-06 18:20:57 +0530

    Update employer.service.ts

--- src/app/Service/employer.service.ts
+++ src/app/Service/employer.service.ts
@@ -9,7 +9,7 @@ import { Application } from '../Model/application';
 })
 export class EmployerService {
 
-  base_url = "http://20.53.113.233:8082/";
+  base_url = "http://48.217.216.9:8082/";
   constructor(private http: HttpClient) {   }
 
   public getJob(Id:number):Observable <any>{
